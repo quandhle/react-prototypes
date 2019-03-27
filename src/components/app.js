4661143d951e875badb75faf01a0ba8638646acb
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-03-26 17:43:42 -0700

    created skeleton of SGT using react

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,17 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    {name: 'Quan', course: 'professional tomato eater', grade: 100},
+    {name: 'David', course: 'V10 climber', grade: 10},
+    {name: 'Jason', course: 'Too nice Jason', grade: 100}
+]
 
+export default () => {
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
