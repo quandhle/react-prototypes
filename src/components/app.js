0276c5b6a0416bf2b780e7d5ec85d64e3421a5c1
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-04-02 10:37:42 -0700

    ignore changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import Welcome from './welcome';
+import Route from 'react-router-dom';
 
-
+export default () => {
+    return (
+        <div className="container">
+            <h1>Home Page</h1>
+            <Route path="/" component={Welcome}></Route>
+        </div>
+    )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>Lorem ipsum blah blah blah</p>
+        </div>
+    )
+}
